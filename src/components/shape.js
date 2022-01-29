2469285f1f3b6459991f7bd0886a313fe71c2070
Madhav Rathi <93340695+madhavrathi16@users.noreply.github.com>
2022-01-30 00:23:40 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
+class Shape{
+  constructor(color){
+    this.color= 'color';
+  }
 
+     drawShape(){
+    console.log('Shape');
+     }
+  calculateArea(){
+    console.log('Area');
+     }
+}
+    
 // export class using module.exports
+  module.exports = Shape;
